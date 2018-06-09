70c8f2a5725d54aac7c67b06304420b21a4d63c8
Moe-Mah <Mohammed-mahhouk@hotmail.com>
2018-06-10 01:16:14 +0200

    mini refinement

--- Statistician-Task4/src/main/Main.java
+++ Statistician-Task4/src/main/Main.java
@@ -10,14 +10,18 @@ import loader.PluginLoader;
 public class Main {
 
 	public static void main(String[] args) {
-		IDataLoader dataLoader = new JsonLoadData("statsData.txt");
+		if (args.length < 1) {
+			throw new IllegalArgumentException("Missing input file");
+		}
+		final String path = args[0];
+		IDataLoader dataLoader = new JsonLoadData(path);
 		List<Double> dataList = dataLoader.loadData();
-		List<IMathOperation> mathOperPlugins = PluginLoader.load(IMathOperation.class);
-		execute(mathOperPlugins, dataList);
+		List<IMathOperation> operations = PluginLoader.load(IMathOperation.class);
+		execute(operations, dataList);
 	}
 	
-	private static void execute(List<IMathOperation> mathOperPlugins, List<Double> dataList) {
-		for (IMathOperation operation : mathOperPlugins) {
+	private static void execute(List<IMathOperation> operations, List<Double> dataList) {
+		for (IMathOperation operation : operations) {
 			System.out.println("The " + operation.getName() + " of these data is : " + operation.execute(dataList));
 		}
 	}
